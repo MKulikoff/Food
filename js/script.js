38f5f123f163f64062725399427e2affb083bf37
Mikhail <makulikovk@gmail.com>
2021-09-23 21:23:20 +0300

    Task 14 Rest

--- js/script.js
+++ js/script.js
@@ -145,12 +145,13 @@ document.addEventListener('DOMContentLoaded', () => {
     //Cards 
 
     class Card {
-         constructor(src, alt, subtitle, description, price, parent) {
+         constructor(src, alt, subtitle, description, price, parent, ...classes) {
             this.src = src; 
             this.alt = alt; 
             this.subtitle = subtitle; 
             this.description = description; 
             this.price = price; 
+            this.classes = classes; 
             this.rubPrice = 72; 
             this.parent = document.querySelector(parent); 
             this.changeToRub(); 
@@ -161,10 +162,19 @@ document.addEventListener('DOMContentLoaded', () => {
          }
 
          createMenuItem() {
+
+            
              const menuItem = document.createElement('div'); 
-             this.parent.append(menuItem); 
              
-             menuItem.innerHTML = `<div class="menu__item">
+             if (this.classes.length === 0) {
+                menuItem.classList.add('menu__item'); 
+             } else {
+                 this.classes.forEach(element => {
+                    menuItem.classList.add('element'); 
+                 });
+             }
+             
+             menuItem.innerHTML = `
              <img src=${this.src} alt=${this.alt}>
              <h3 class="menu__item-subtitle">${this.subtitle}</h3>
              <div class="menu__item-descr">${this.description}</div>
@@ -172,8 +182,9 @@ document.addEventListener('DOMContentLoaded', () => {
              <div class="menu__item-price">
                  <div class="menu__item-cost">Цена:</div>
                  <div class="menu__item-total"><span>${this.price}</span> рублей/день</div>
-             </div>
-         </div>`
+             </div>`;
+
+         this.parent.append(menuItem); 
          }
     }
 
